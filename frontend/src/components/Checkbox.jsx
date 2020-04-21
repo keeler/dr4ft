c44e2baf94ab47add48ab21db07a8115cf5b5b1d
Keeler Russell <keeler.russell@gmail.com>
2020-04-20 19:59:20 -0700

    refactor: Extract Checkbox.jsx

---
+++ frontend/src/components/Checkbox.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import App from "../app";
+
+export const Checkbox = ({link, text, side, onChange, ...rest}) => (
+  <div>
+    {side === "right" ? text : ""}
+    <input
+      {...rest}
+      type="checkbox"
+      onChange={onChange || function (e) {
+        App.save(link, e.currentTarget.checked);
+      }}
+      checked={App.state[link]}/>
+    {side === "left" ? text : ""}
+  </div>
+);
+
+Checkbox.propTypes = {
+  link: PropTypes.string,
+  text: PropTypes.string,
+  side: PropTypes.string,
+  onChange: PropTypes.func
+};
